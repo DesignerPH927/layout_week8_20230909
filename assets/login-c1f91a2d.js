0cc19fdf297a3d46470ee27e0e08a5d51ce72940
DesignerPH927 <designer.ph927@gmail.com>
2023-09-09 02:15:53 +0800

    Updates

---
+++ assets/login-c1f91a2d.js
@@ -0,0 +1,1 @@
+import"./main-32fc542e.js";const e=document.querySelector(".circleText");e.innerHTML=e.innerText.split("").map((t,n)=>`<span style="transform: rotate(${n*20}deg)">${t}</span>`).join("");
